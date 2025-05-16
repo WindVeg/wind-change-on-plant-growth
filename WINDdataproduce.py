9d6ab22cda29550d087d196680d5d00408801aa2
WindVeg <mixiyuan24@mails.ucas.ac.cn>
2025-05-16 21:00:14 +0800

    Update WINDdataproduce.py

--- WINDdataproduce.py
+++ WINDdataproduce.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -12,3 +11,11 @@ pre.to_netcdf('G:/CN05.1/month/CN05.1_Win_1961_2022_monthly_025x025.nc')
 
 
 f1=xr.open_dataset('G:/CN05.1/month/CN05.1_Win_1961_2022_monthly_025x025.nc')
+
+
+
+f=xr.open_dataset('G:/CN05.1/CN05.1_Win_1961_2022_daily_025x025.nc')
+f= f.sel(time=slice('1982', '2020'))
+pre=f.win
+pre = pre.resample(time='1YE').mean()
+pre.to_netcdf('G:/CN05.1/month/CN05.1_Win_1982_2020_yearly_025x025.nc') 
