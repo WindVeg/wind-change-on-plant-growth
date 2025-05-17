07dc8812ac96715ac86590f163e33cd4dba097d0
WindVeg <mixiyuan24@mails.ucas.ac.cn>
2025-05-17 09:22:13 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import xarray as xr
-import matplotlib.pyplot as plt
-import pwlf
-import cartopy.crs as ccrs
-import geopandas as gpd
-import salem
-import rpy2.robjects as robjects
